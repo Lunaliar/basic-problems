3f067762bf1423bf30acb08027f10091c1fd446d
Jake Tripp <shmogens@gmail.com>
2023-01-08 15:27:00 -0800

    algos

--- basicAlgorithmScipting.js
+++ basicAlgorithmScipting.js
@@ -25,37 +25,106 @@ function factorialize(num) {
 	return num;
 }
 
+// function factorialize(num) {
+// 	if (num === 0 || num === 1) return 1;
+// 	let total = num
+// 	for (let i = num - 1; i > 0; i--) {
+// 		total = total * i;
+// 	}
+// 	return total;
+// }
+
 factorialize(5);
 
 //! /////////////////////////////////////////////////////////////
 
+// function findLongestWordLength(str) {
+// 	const arr = str.split(" ");
+// 	let word = "";
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i].length > word.length) word = arr[i];
+// 	}
+// 	return word.length;
+// }
+
+// function findLongestWordLength(str) {
+// 	return str.split(" ").reduce((previouslyLongestWordLength, currentWord) => {
+// 		return Math.max(previouslyLongestWordLength, currentWord.length)
+// 	}, 0)
+// 	// we want the longest word length from the array of words
+// }
+
 function findLongestWordLength(str) {
-	const arr = str.split(" ");
-	let word = "";
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i].length > word.length) word = arr[i];
-	}
-	return word.length;
+	return str
+		.split(" ")
+		.reduce((longestWordSoFar, currentWord) => {
+			return longestWordSoFar.length > currentWord.length ? longestWordSoFar : currentWord
+		}).length
+	// we want the longest word length from the array of words
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
 //! /////////////////////////////////////////////////////////////
 
-function largestOfFour(arr) {
-	const results = [];
-	for (let i = 0; i < arr.length; i++) {
-		let largestNumber = arr[i][0];
-		for (let j = 1; j < arr[i].length; j++) {
-			if (arr[i][j] > largestNumber) {
-				largestNumber = arr[i][j];
-			}
-		}
-		results[i] = largestNumber;
-	}
+// function largestOfFour(arr) {
+// 	const results = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		let largestNumber = arr[i][0];
+// 		for (let j = 1; j < arr[i].length; j++) {
+// 			if (arr[i][j] > largestNumber) {
+// 				largestNumber = arr[i][j];
+// 			}
+// 		}
+// 		results[i] = largestNumber;
+// 	}
 
-	return results;
+// 	return results;
+// }
+
+// YUCK!
+// function largestOfFour(arr) {
+// 	const firstValuesOfEachArray = arr.map(subArray => subArray[0])
+// 	return arr.reduce((longestValuesOfEachArray, currentArray, i, allArrays) => {
+// 		let longestValueOfCurrentArray = longestValuesOfEachArray[i]
+// 		for (let j = 0; j < currentArray.length; j++) {
+// 			if (currentArray[j] > longestValueOfCurrentArray) {
+// 				longestValueOfCurrentArray = currentArray[j]
+// 			}
+// 		}
+// 		const newLongestValuesOfEachArray = longestValuesOfEachArray.slice()
+// 		newLongestValuesOfEachArray[i] = longestValueOfCurrentArray
+// 		return newLongestValuesOfEachArray
+// 	}, firstValuesOfEachArray)
+// }
+
+// function largestOfFour(nestedArray) {
+// 	return nestedArray.map(currentArray => {
+// 		return currentArray.reduce((largestSoFar, currentValue) => {
+// 			return Math.max(largestSoFar, currentValue)	
+// 		})
+// 	})
+// }
+
+// O(n * m) aka the number of subarrays * the length of the longest subarray
+function largestOfFour(nestedArray) {
+	return nestedArray.map(currentArray => Math.max(...currentArray))
 }
+
+
+// an array of arrays
+// an array
+// the largest value (aka the max)
+
+// [4,13,32,1000]
+// [4,5,1,3]
+// 0
+// [
+//  [4, 5, 1, 3],
+// 	[13, 27, 18, 26],
+// 	[32, 35, 37, 39],
+// 	[1000, 1001, 857, 1],
+// ]
 
 console.log(
 	largestOfFour([
@@ -65,6 +134,7 @@ console.log(
 		[1000, 1001, 857, 1],
 	])
 );
+// [?,?,?,?]
 
 //! /////////////////////////////////////////////////////////////
 
@@ -76,24 +146,30 @@ confirmEnding("Bastian", "n");
 
 //! /////////////////////////////////////////////////////////////////
 
-function repeatStringNumTimes(str, num) {
-	if (num === 0) return "";
-	else {
-		let newStr = "";
-		for (let i = num; i > 0; i--) {
-			newStr = newStr + str;
-		}
-		return newStr;
-	}
+// function repeatStringNumTimes(str, num) {
+// 	if (num === 0) return "";
+// 	else {
+// 		let newStr = "";
+// 		for (let i = num; i > 0; i--) {
+// 			newStr = newStr + str;
+// 		}
+// 		return newStr;
+// 	}
+// }
+
+function repeatStringNumTimes(str, length) {
+	return Array.from({ length }).map(_ => str).join("");
 }
 
 repeatStringNumTimes("abc", 3);
 
 //! /////////////////////////////////////////////////////////////
 function truncateString(str, num) {
-	if (str.length <= num) return str;
-	str = str.substring(0, num) + "...";
-	return str;
+	if (str.length <= num) {
+		return str;
+	} else {
+		return str.substring(0, num) + "...";
+	}
 }
 
 console.log(
@@ -105,14 +181,18 @@ console.log(
 
 //! /////////////////////////////////////////////////////////////
 
+// function findElement(arr, func) {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (func(arr[i])) return arr[i];
+// 	}
+// 	return undefined;
+// } 
+
 function findElement(arr, func) {
-	for (let i = 0; i < arr.length; i++) {
-		if (func(arr[i])) return arr[i];
-	}
-	return undefined;
+	return arr.filter(x => func(x))[0]
 }
 
-findElement([1, 3, 5, 8, 9, 10], (num) => num % 2 === 0);
+// findElement([1, 3, 5, 8, 9, 10], (num) => num % 2 === 0);
 
 //! /////////////////////////////////////////////////////////////
 
@@ -188,6 +268,10 @@ function bouncer(arr) {
 	return trueArr;
 }
 
+function bouncer(arr) {
+	return arr.filter(Boolean)
+}
+
 bouncer([7, "ate", "", false, 9]);
 
 //! /////////////////////////////////////////////////////////////
@@ -204,18 +288,38 @@ function getIndexToIns(arr, num) {
 	return arr.indexOf(min) + 1;
 }
 
-getIndexToIns([10, 20, 30, 40, 50], 30);
-//getIndexToIns([40, 60, 32, 80], 50);
+// getIndexToIns([10, 20, 30, 40, 50], 30);
+getIndexToIns([40, 60, 32, 80], 50);
+// [40, 60, 32, 80]
+// [32, 40, 60, 80]
+// [32, 40, 50, 60, 80]
+// output is 2
+getIndexToIns([1, 2, 3, 4], 0); // 0
+getIndexToIns([1, 2, 3, 3, 3, 4], 3); // 5
+getIndexToIns([1, 2, 3, 4], 5); // 4
+
+
 
 //! /////////////////////////////////////////////////////////////
 
-function mutation(arr) {
-	const target = arr[0].toLowerCase().split("");
-	const test = arr[1].toLowerCase().split("");
-	for (let i = 0; i < test.length; i++) {
-		if (target.indexOf(test[i]) < 0) return false;
-	}
-	return true;
+// function mutation(arr) {
+// 	const target = arr[0].toLowerCase().split("");
+// 	const test = arr[1].toLowerCase().split("");
+// 	for (let i = 0; i < test.length; i++) {
+// 		if (target.indexOf(test[i]) < 0) return false;
+// 	}
+// 	return true;
+// }
+
+// string
+// number
+// boolean
+// Array
+// Object
+// Set
+function mutation([target, test]) {
+	const targetSet = new Set(target.toLowerCase().split(''))
+	return test.toLowerCase().split("").every(character => targetSet.has(character))
 }
 
 mutation(["hello", "hey"]);
