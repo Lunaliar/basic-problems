4920d9b44d92fb6f78cae9218b8283b8e3599f7c
sav <savcostabile@gmail.com>
2023-01-18 12:27:24 -0500

    update

--- debug.js
+++ debug.js
@@ -1,16 +1,18 @@
 function orbitalPeriod(satellites) {
-  debugger
-  const GM = 398600.4418;
-  const ER = 6367.4447;
+	debugger;
+	const GM = 398600.4418;
+	const ER = 6367.4447;
 
-  return satellites.map(({ avgAlt, name }) => {
-    const axis = avgAlt + ER;
-    const orbitalPeriod = Math.round((2 * Math.PI) * (Math.sqrt(Math.pow(axis, 3) / GM)));
-    return {
-      name,
-      orbitalPeriod
-    }
-  })
+	return satellites.map(({ avgAlt, name }) => {
+		const axis = avgAlt + ER;
+		const orbitalPeriod = Math.round(
+			2 * Math.PI * Math.sqrt(Math.pow(axis, 3) / GM)
+		);
+		return {
+			name,
+			orbitalPeriod,
+		};
+	});
 }
 
 orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
