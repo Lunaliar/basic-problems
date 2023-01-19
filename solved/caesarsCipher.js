34c4905d284c5f341072743945b6d3917bb0c078
sav <savcostabile@gmail.com>
2023-01-19 12:06:02 -0500

    cipher

--- solved/caesarsCipher.js
+++ solved/caesarsCipher.js
@@ -17,3 +17,23 @@ rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
 rot13("SERR YBIR?") should decode to the string FREE LOVE?
 rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
 */
+const alphabet = () => {
+	const codes = Array.from(Array(26)).map((e, i) => i + 65);
+	return codes.map((x) => String.fromCharCode(x).toUpperCase());
+};
+
+function rot13(str) {
+	const letters = alphabet();
+	const cipher = alphabet().slice(13).concat(alphabet().slice(0, 13));
+	const decodedWords = str.split(" ").map((word) => {
+		const decodedWord = word.split("").map((letter) => {
+			const decodedLetter = letters[cipher.indexOf(letter)];
+			return decodedLetter ? decodedLetter : letter;
+		});
+		return decodedWord.join("");
+	});
+	return decodedWords.join(" ");
+}
+
+rot13("SERR PBQR PNZC");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.");
