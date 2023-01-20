7583570b37fbc23b6e3b6ac4bff03d3d037e904b
sav <savcostabile@gmail.com>
2023-01-20 11:18:08 -0500

    roman defeat

--- romanNumerals.js
+++ romanNumerals.js
@@ -45,3 +45,32 @@ convertToRoman(1023) should return the string MXXIII
 convertToRoman(2014) should return the string MMXIV
 convertToRoman(3999) should return the string MMMCMXCIX
 */
+
+const romanNumerals = {
+	M: 1000,
+	CM: 900,
+	D: 500,
+	CD: 400,
+	C: 100,
+	XC: 90,
+	L: 50,
+	XL: 40,
+	X: 10,
+	IX: 9,
+	V: 5,
+	IV: 4,
+	I: 1,
+};
+
+const convertToRoman = (num) => {
+	let roman = "",
+		key;
+	for (key in romanNumerals) {
+		while (num >= romanNumerals[key]) {
+			roman += key;
+			num -= romanNumerals[key];
+		}
+	}
+	return roman;
+};
+convertToRoman(36);
